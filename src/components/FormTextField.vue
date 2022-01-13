95e5bf5621f888fb2ca46e50ba49c080ddd15c0f
Ali Mirmohammad <ali.mirmohammad.86@gmail.com>
2022-01-13 12:12:12 +0330

    create a text-field component with validation logic

---
+++ src/components/FormTextField.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <validation-provider :name="name" :rules="rules" v-slot="{ errors }">
+      <label :for="name">{{ label }}</label>
+      <b-form-input
+        @input="handleInput"
+        :value="value"
+        :state="errors.length > 0 ? false : null"
+        :id="name"
+        :type="type"
+      ></b-form-input>
+      <b-form-invalid-feedback :state="errors.length > 0 ? false : null">
+        {{ errors[0] }}
+      </b-form-invalid-feedback>
+    </validation-provider>
+  </div>
+</template>
+
+<script>
+import { ValidationProvider } from "vee-validate";
+
+export default {
+  name: "FormTextField",
+  props: ["name", "rules", "label", "value", "type"],
+  components: { ValidationProvider },
+  methods: {
+    handleInput(text) {
+      this.$emit("input", text);
+    },
+  },
+};
+</script>
