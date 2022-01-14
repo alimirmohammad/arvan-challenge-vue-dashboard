3f0ece6d4bf96de005b0f57234b0cb3a73438816
Ali Mirmohammad <ali.mirmohammad.86@gmail.com>
2022-01-14 11:59:44 +0330

    add route guards

---
+++ src/constants/routes.js
@@ -0,0 +1,6 @@
+export const ROUTES = {
+  REGISTER_ROUTE: "/register",
+  LOGIN_ROUTE: "/login",
+  ARTICLES_ROUTE: "/articles",
+  CATCH_ALL_ROUTE: "*",
+};
--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,35 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import { ROUTES } from "../constants/routes";
 import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
+import Articles from "../views/Articles.vue";
+import store from "../store/index";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/login",
-    name: "Login",
+    path: ROUTES.LOGIN_ROUTE,
     component: Login,
+    meta: { onlyUnauthenticated: true },
   },
   {
-    path: "/register",
-    name: "Register",
+    path: ROUTES.REGISTER_ROUTE,
     component: Register,
+    meta: { onlyUnauthenticated: true },
+  },
+  {
+    path: ROUTES.ARTICLES_ROUTE,
+    component: Articles,
+    meta: { onlyAuthenticated: true },
+  },
+  {
+    path: ROUTES.CATCH_ALL_ROUTE,
+    redirect() {
+      const isAuthenticated = store.getters.isAuthenticated;
+      return isAuthenticated ? ROUTES.ARTICLES_ROUTE : ROUTES.LOGIN_ROUTE;
+    },
   },
 ];
 
@@ -28,6 +37,19 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach(function (to, from, next) {
+  const isAuthenticated = store.getters.isAuthenticated;
+  if (to.meta.onlyUnauthenticated) {
+    if (isAuthenticated) next(ROUTES.ARTICLES_ROUTE);
+    else next();
+  } else if (to.meta.onlyAuthenticated) {
+    if (isAuthenticated) next();
+    else next(ROUTES.LOGIN_ROUTE);
+  } else {
+    next();
+  }
 });
 
 export default router;
