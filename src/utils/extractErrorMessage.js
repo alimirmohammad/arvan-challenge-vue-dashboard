316751602ce9be3992f929bfdeee7ec1fb45503a
Ali Mirmohammad <ali.mirmohammad.86@gmail.com>
2022-01-15 23:51:22 +0330

    improve extract error to use with message field

--- src/utils/extractErrorMessage.js
+++ src/utils/extractErrorMessage.js
@@ -1,6 +1,6 @@
 export default function extractErrorMessage(error) {
   try {
-    if (error.message) return error.message;
+    if (error.data.message) return error.data.message;
     const [key, value] = Object.entries(error.data.errors)[0];
     return `${key.charAt(0).toUpperCase()}${key.slice(1)} ${value}`;
   } catch (error) {
