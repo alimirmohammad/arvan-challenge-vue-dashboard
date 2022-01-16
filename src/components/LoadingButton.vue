bc289e36fd276ee9a3c4411f02e4d9413d96fe03
Ali Mirmohammad <ali.mirmohammad.86@gmail.com>
2022-01-16 19:31:57 +0330

    create loading button component

---
+++ src/components/LoadingButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <b-button
+    variant="primary"
+    class="submit-btn"
+    :type="type"
+    :disabled="loading"
+  >
+    <b-spinner v-if="loading" variant="light" label="loading" small></b-spinner>
+    <span v-else>{{ label }}</span>
+  </b-button>
+</template>
+
+<script>
+export default {
+  name: "LoadingButton",
+  props: ["loading", "type", "label"],
+};
+</script>
+
+<style lang="scss" scoped>
+.submit-btn {
+  width: 100px;
+}
+</style>
